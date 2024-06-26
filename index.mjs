7c22e7d3d8eb69ee86441ea7d547847bc3b3ea63
AneS279 <anes.2017@alunos.utfpr.edu.br>
2024-06-25 22:03:28 -0300

    test: search for objects s3

--- index.mjs
+++ index.mjs
@@ -1,9 +1,25 @@
 import { log } from "./log.mjs";
+import {S3} from '@aws-sdk/client-s3'
 
+const s3Client = new S3({region: 'us-east-1'})
 export const handler = async (event) => {
-  log('event: ' + JSON.stringify(event))
-  return {
-    statusCode: 200,
-    body: JSON.stringify(event),
-  };;
-};
+  const record = event.record[0]
+  const Bucket = record.s3.bucket.name;
+  const Key = record.s3.object.key
+  const getObjectResult = await s3Client.getObject({
+    Bucket,
+    Key
+  })
+
+  const mega_byte = 1024*1024
+
+  if(getObjectResult.ContentLength > mega_byte){
+      log('Objeto muito grande')
+      return 'Objeto muito grande'
+  }
+  else{
+      log('Objeto OK')
+      return 'Objeto OK'
+  
+  }
+  };
