f755995b2ad0356213ca1beef7171ae8d7852f73
AneS279 <anes.2017@alunos.utfpr.edu.br>
2024-06-25 22:10:29 -0300

    test: fix misspell-2

--- index.mjs
+++ index.mjs
@@ -3,7 +3,7 @@ import {S3} from '@aws-sdk/client-s3'
 
 const s3Client = new S3({region: 'us-east-1'})
 export const handler = async (event) => {
-  const record = event.records[0]
+  const record = event.Records[0]
   const Bucket = record.s3.bucket.name;
   const Key = record.s3.object.key
   const getObjectResult = await s3Client.getObject({
